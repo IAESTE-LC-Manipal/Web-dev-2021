bd331e8bfcbc2575271611bb21eec86caf1b6e1d
Ishita1210 <76723911+Ishita1210@users.noreply.github.com>
2021-06-06 23:24:33 +0530

    Add files via upload

---
+++ week3/work.js
@@ -0,0 +1,22 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+  
+  // menu//
+  function toggleSidebar(ref){
+    document.getElementById("sidebar").classList.toggle('active');
+  }
+  
+
+
+
+
+
+
+  
